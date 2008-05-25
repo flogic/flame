ecbc3618f742513260d9838320f0d234c37ed452
Rick Bradley <rick@rickbradley.com>
2008-05-24 22:48:32 -0500

    adding tests for the remainder of the command-line options for command-line flog binary.  Note that two pending specs are added to deal with a failure to display usage when an invalid -I option is given.  Note that the ruby -s method of dealing with args is less than ideal, and any args that are used internally by the library that aren't -h or -I are completely untested, and only partially documented.  Bad mojo.

--- spec/flog_command_spec.rb
+++ spec/flog_command_spec.rb
@@ -2,26 +2,26 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 require 'flog'
 
 describe 'flog command' do
+  before :all do
+    path = File.join(File.dirname(__FILE__), *%w[.. bin])
+    ENV['PATH'] = [path, ENV['PATH']].join(':')
+  end
+
+  before :each do
+    @flog = stub('Flog', :flog_files => true, :report => true)
+    Flog.stubs(:new).returns(@flog)
+    self.stubs(:exit)
+  end
+
   def run_command
     eval File.read(File.join(File.dirname(__FILE__), *%w[.. bin flog]))
-  rescue SystemExit
   end
   
   describe 'when no command-line arguments are specified' do
-    before :all do
-      path = File.join(File.dirname(__FILE__), *%w[.. bin])
-      ENV['PATH'] = [path, ENV['PATH']].join(':')
-    end
-  
     before :each do
       Object.send(:remove_const, :ARGV)
       ARGV = []
     end  
-  
-    before :each do
-      @flog = stub('Flog', :flog_files => true, :report => true)
-      Flog.stubs(:new).returns(@flog)
-    end
   
     it 'should run' do
       lambda { run_command }.should_not raise_error(Errno::ENOENT)
@@ -45,6 +45,88 @@ describe 'flog command' do
     it "should pass '-' (for the file path) to flog_files on the instance" do
       @flog.expects(:flog_files).with(['-'])
       run_command
+    end
+  end
+  
+  describe "since the script assumes it was called with 'ruby -s'" do
+    describe "when -h is specified on the command-line" do
+      before :each do
+        Object.send(:remove_const, :ARGV)
+        ARGV = []
+        $h = true  # ruby -s, ftw
+      end
+      
+      after :each do
+        $h = nil
+      end
+      
+      before :each do
+        self.stubs(:puts).returns(nil)
+      end
+    
+      it "should display help information" do
+        self.expects(:puts).at_least_once
+        run_command
+      end
+      
+      it 'should not create a Flog instance' do
+        Flog.expects(:new).never
+        run_command
+      end
+      
+      it 'should exit with status 0' do
+        self.expects(:exit).with(0)
+        run_command
+      end
+    end
+    
+    describe 'when -I is specified on the command-line' do
+      before :each do
+        Object.send(:remove_const, :ARGV)
+        ARGV = []
+      end
+      
+      before :each do
+        @paths = $:.dup
+      end
+      
+      describe 'when -I is not given a string' do
+        before :each do
+          $I = 234  # ruby -s, ftw
+        end
+        
+        after :each do
+          $I = nil
+        end
+        
+        it '(currently) should not modify the include path' do
+          run_command
+          $:.should == @paths
+        end
+        
+        it 'should display usage'
+        it 'should not create a Flog instance'
+      end
+      
+      describe 'when -I is given a string' do
+        before :each do
+          $I = '/bin/true'      
+        end
+        
+        after :each do
+          $I = nil
+        end
+        
+        it "should append each ':' separated path to $:" do
+          run_command
+          $:.should_not == @paths
+        end
+      end
+      
+      it 'should create a Flog instance' do
+        Flog.expects(:new).returns(@flog)
+        run_command
+      end
     end
   end
 end
