b54505f6f532557797df2e7bbf7df5586d33b72a
Rick Bradley <rick@rickbradley.com>
2008-05-28 17:29:23 -0500

    adding very basic specs to the Flog#initialize method

--- spec/flog_spec.rb
+++ spec/flog_spec.rb
@@ -2,5 +2,24 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 require 'flog'
 
 describe Flog do
-  it 'should have some goddamn specs'
+  describe 'after initializing' do
+    before :each do
+      @flog = Flog.new
+    end
+
+    it 'may need to verify more state than these specs currently do'
+    
+    it 'should return an SexpProcessor' do
+      @flog.should be_a_kind_of(SexpProcessor)
+    end
+    
+    it 'should be initialized like all SexpProcessors' do
+      # less than ideal means of insuring the Flog instance was initialized properly, imo -RB
+      @flog.context.should == []  
+    end
+    
+    it 'should not have any calls yet' do
+      @flog.calls.should == {}
+    end
+  end
 end
