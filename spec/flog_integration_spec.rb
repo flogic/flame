91b6e5185c2d9489fbadba72e3a17e95a0a21832
Rick Bradley <rick@rickbradley.com>
2008-07-30 19:52:09 -0500

    adding integration spec assertions for overall 'total' behavior on our fixtures

--- spec/flog_integration_spec.rb
+++ spec/flog_integration_spec.rb
@@ -26,6 +26,11 @@ describe Flog do
       it 'should not fail when flogging the given input' do
         lambda { @flog.flog_files(fixture_files(@files)) }.should_not raise_error
       end
+      
+      it 'should report an overall flog score of 0' do
+        @flog.flog_files(fixture_files(@files))
+        @flog.total.should be_close(0.0, 0.0000000001)
+      end
     end
   
     describe 'when given a simple file' do
@@ -35,6 +40,11 @@ describe Flog do
     
       it 'should not fail when flogging the given input' do
         lambda { @flog.flog_files(fixture_files(@files)) }.should_not raise_error
+      end
+
+      it 'should report an overall flog score of 235.694161066381' do
+        @flog.flog_files(fixture_files(@files))
+        @flog.total.should be_close(235.694161066381, 0.0000000001)
       end
     end
   
@@ -46,6 +56,11 @@ describe Flog do
       it 'should not fail when flogging the given input' do
         lambda { @flog.flog_files(fixture_files(@files)) }.should_not raise_error
       end
+
+      it 'should report an overall flog score of 209.977217342726' do
+        @flog.flog_files(fixture_files(@files))
+        @flog.total.should be_close(209.977217342726, 0.0000000001)        
+      end
     end
   
     describe 'when given a collection of files' do
@@ -55,6 +70,11 @@ describe Flog do
 
       it 'should not fail when flogging the given input' do
         lambda { @flog.flog_files(fixture_files(@files)) }.should_not raise_error
+      end
+
+      it 'should report an overall flog score of 981.137760580242' do
+        @flog.flog_files(fixture_files(@files))
+        @flog.total.should be_close(981.137760580242, 0.0000000001)
       end
     end
   end
